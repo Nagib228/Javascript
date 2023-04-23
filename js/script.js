c0f3693b2c0de3de0702471e8200dfc37e3bda94
Oleg <oleg25111@inbox.ru>
2023-04-23 14:18:07 +0300

    Methods and properties

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 // Function Declaration
 
-let num = 20;
+/* let num = 20;
 
 function showFirstMessage() {
     console.log("Hello World!");
@@ -34,16 +34,55 @@ function ret() {
     return num;
 }
 const anotherNum = ret();
-console.log(anotherNum);
+console.log(anotherNum); */
 
 // Function Expression
 
-const logger = function () {
+/* const logger = function () {
     console.log("World!");
 };
-logger();
+logger(); */
 
 // Стрелочная функция
 
-const calc = (a, b) => {return a + b};
+/* const calc = (a, b) => {return a + b};
 console.log(calc(3, 10));
+
+ */
+
+
+// Методы и свойства
+// Свойтство
+
+const str = "Hello world";
+console.log(str.length);
+
+const array = [1, 2, 4, 5];
+console.log(array.length);
+
+//Методы
+
+console.log(str.toLocaleUpperCase());
+console.log(str.toLocaleLowerCase());
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+console.log(fruit.indexOf("z"));  //нет значения
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));  //запись "ДО" не включительна 
+console.log(logg.slice(6));
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+
+const num = 12.99;
+console.log(Math.round(num));
+console.log(Math.floor(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
