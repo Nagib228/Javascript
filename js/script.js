9b329ce7cfb8b324caa1dee3c9358d681d589ba0
Oleg <oleg25111@inbox.ru>
2023-04-23 16:45:21 +0300

    Objects and Destructure

--- js/script.js
+++ js/script.js
@@ -27,3 +27,55 @@ function done () {
 }
 
 learnJS(`JavaScript`, done);  //done не вызываем,а обращаемся к имени
+
+// Объекты, деструктуризация объектов
+
+//const obj = new Object();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        bg: "red"
+    }
+};
+console.log(options.name);
+
+/* delete options.name;
+console.log(options); */
+
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+}
+
+console.log(options["colors"]["border"]);
+
+let counter = 0;
+for (let j in options) {
+    counter++;
+}
+console.log(counter);
+
+console.log(Object.keys(options));   //Создаём массив из ключей объекта
+console.log(Object.keys(options).length); //количество ключей первой вложенности
+
+
+const method = {
+    makeTest: function () {
+        console.log("Test");
+    }
+};
+method.makeTest();
+
+//Деструктуризация
+
+const {border, bg} = options.colors;
+console.log(border);
