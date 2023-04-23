99d49fb6be74e6bc866545a0e72f8eb18a279a86
Oleg <oleg25111@inbox.ru>
2023-04-23 15:21:05 +0300

    Callback

--- js/script.js
+++ js/script.js
@@ -1,63 +1,29 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?"," ");
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?"," ");
-    }
+function first() {
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
-start();
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Один из просмотренных фильмов?", "");
-        while (a == "" || a == null || a.length > 50){
-            a = prompt("Один из просмотренных фильмов?", "");
-        }
-        let b = prompt("На сколько оцените его?", "");
-        while (b == "" || b == null || b.length > 50){
-            b = prompt("На сколько оцените его?", "");
-        }
-        personalMovieDB.movies[a] = b;
-    }
+function second() {
+    console.log(2);
 }
-//rememberMyFilms();
 
-function writeYourGenres (genre) {
-    for (let i = 1; i <= 3; i++) {
-        genre[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
+first();
+second();
+
+function learnJS (lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
-writeYourGenres(personalMovieDB.genres);
 
+learnJS(`Javascript`, function() {
+    console.log(`Я прошёл этот урок!`);
+});
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Ошибка");
-    }
+function done () {
+    console.log(`Я прошёл этот урок!`);
 }
-detectPersonalLevel();
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
+learnJS(`JavaScript`, done);  //done не вызываем,а обращаемся к имени
