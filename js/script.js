1e1fbbd59506ea5ec73423ac883c42f77d7fd3db
Oleg <oleg25111@inbox.ru>
2023-04-28 16:50:44 +0300

    Insert Objects && InnerHTML

--- js/script.js
+++ js/script.js
@@ -1,26 +1,71 @@
 "use strict";
 
-const box = document.getElementById('box');
-console.log(box);
-/* 
-const btns = document.getElementsByTagName('button');
-console.log(btns);
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      hearts = document.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
+
+console.dir(box);
+
+/* box.style.backgroundColor = 'blue';
+box.style.width = '500px'; */
+
+btns[1].style.borderRadius = '100%'; //Сделать овал
+circles[0].style.backgroundColor = 'red';
+
+// box.style.cssText = 'background-color: blue; width: 500px';
+
+const someColor = 'blue',
+    num = '500px';
+box.style.cssText = `background-color: ${someColor}; width: ${num}`;
+
+/* for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.backgroundColor = 'green';
+}
  */
-const btns  = document.getElementsByTagName('button')[0];
-console.log(btns);   // console.log(btns[0]);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-// Новый стандарт
-
-const hearts = document.querySelectorAll('.heart');         // Появлятеся метод forEach
-hearts.forEach((item, i, hearts) => {
-    console.log(item);
+hearts.forEach((item, i) => {
+    item.style.backgroundColor = 'green';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+// Create elements
 
-const selectDiv = document.querySelector('div');
-console.log(selectDiv);
+const div = document.createElement('div'),
+      text = document.createTextNode('Яп яп'),
+      wrapper = document.querySelector('.wrapper');
+
+div.classList.add('black');
+
+// document.body.append(div);
+wrapper.append(div);
+//wrapper.prepend(div);
+
+//hearts[0].before(div);  
+//hearts[0].after(div);
+
+//circles[0].remove();
+
+//hearts[0].replaceWith(circles[0]);
+
+// Old constructions
+/* 
+wrapper.appendChild(div);
+wrapper.insertBefore(div, hearts[0]);
+wrapper.removeChild(hearts[1]);
+wrapper.replaceChild(circles[0], hearts[0]); 
+*/
+
+// inneHTML
+div.innerHTML = "<h1>Hello, my friend</h1>";
+
+//TextContent
+//div.textContent = "Hello";
+
+div.insertAdjacentHTML("afterend", "<h2>Horse</h2>");
+
+// The same
+/* 
+const wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
+ */
