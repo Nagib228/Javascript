7bded7aeef4f78acda48160bc0bdc6c2141cb07d
Oleg <oleg25111@inbox.ru>
2023-04-28 15:33:10 +0300

    Селекторы

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-'use strict';
+"use strict";
 
+const box = document.getElementById('box');
+console.log(box);
+/* 
+const btns = document.getElementsByTagName('button');
+console.log(btns);
+ */
+const btns  = document.getElementsByTagName('button')[0];
+console.log(btns);   // console.log(btns[0]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+// Новый стандарт
+
+const hearts = document.querySelectorAll('.heart');         // Появлятеся метод forEach
+hearts.forEach((item, i, hearts) => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
+
+const selectDiv = document.querySelector('div');
+console.log(selectDiv);
