fd15a6c02dcda7977c0df4d01b02a5d9d61d7e59
Oleg <oleg25111@inbox.ru>
2023-04-30 16:01:30 +0300

    Обработка событий

--- js/script.js
+++ js/script.js
@@ -1,71 +1,26 @@
 "use strict";
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart');
-
-console.dir(box);
-
-/* box.style.backgroundColor = 'blue';
-box.style.width = '500px'; */
-
-btns[1].style.borderRadius = '100%'; //Сделать овал
-circles[0].style.backgroundColor = 'red';
-
-// box.style.cssText = 'background-color: blue; width: 500px';
-
-const someColor = 'blue',
-    num = '500px';
-box.style.cssText = `background-color: ${someColor}; width: ${num}`;
-
-/* for (let i = 0; i < hearts.length; i++) {
-    hearts[i].style.backgroundColor = 'green';
-}
+const btn = document.querySelector('button');
+/* btn.addEventListener('click', () => {
+    alert("Башкирия на связи");
+});
  */
-hearts.forEach((item, i) => {
-    item.style.backgroundColor = 'green';
+btn.addEventListener('mouseenter', (event) => {
+    //console.log("Возьми мобилу, если она не в ....");
+    //console.log(event);
+    console.log(event.target);
+    //event.target.remove();
 });
 
-// Create elements
+const deleteElement = (e) => {
+    e.target.remove();
+};
 
-const div = document.createElement('div'),
-      text = document.createTextNode('Яп яп'),
-      wrapper = document.querySelector('.wrapper');
+btn.addEventListener('click', deleteElement, {once: true});
+btn.removeEventListener('click', deleteElement);
 
-div.classList.add('black');
-
-// document.body.append(div);
-wrapper.append(div);
-//wrapper.prepend(div);
-
-//hearts[0].before(div);  
-//hearts[0].after(div);
-
-//circles[0].remove();
-
-//hearts[0].replaceWith(circles[0]);
-
-// Old constructions
-/* 
-wrapper.appendChild(div);
-wrapper.insertBefore(div, hearts[0]);
-wrapper.removeChild(hearts[1]);
-wrapper.replaceChild(circles[0], hearts[0]); 
-*/
-
-// inneHTML
-div.innerHTML = "<h1>Hello, my friend</h1>";
-
-//TextContent
-//div.textContent = "Hello";
-
-div.insertAdjacentHTML("afterend", "<h2>Horse</h2>");
-
-// The same 
-/* 
-const wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
- */
+const link = document.querySelector('a');
+link.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e.target);
+});
