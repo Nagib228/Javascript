3098d1ff8707381208822f09e1df02c62bf741f2
Oleg <oleg25111@inbox.ru>
2023-05-02 17:03:29 +0300

    Practice 5

--- js/script.js
+++ js/script.js
@@ -38,10 +38,71 @@ poster.style.backgroundImage = 'url("img/bg.jpg")';
 movieList.innerHTML = "";
 movieDB.movies.sort();
 
-movieDB.movies.forEach((film, i) => {           // a = a + 1 => a += 1
+movieDB.movies.forEach((film, i) => { 
     movieList.innerHTML +=   `  
         <li class="promo__interactive-item">${i+1}. ${film}
         <div class="delete"></div>
         </li>
     `;         
+});
+
+const a = document.querySelector('.promo__interactive-title');
+const conf = document.querySelector('button');
+const checkPoint = document.querySelector('.isChecked');
+const remover = document.querySelectorAll('.delete');
+
+a.addEventListener('mouseenter', () => {
+   a.style.color = '#FFD500';
+});
+a.addEventListener("mouseout", () => {
+    a.style.color = 'black';
+});
+
+conf.addEventListener('click' , (e) => {
+    e.preventDefault();
+    const input = document.querySelector('.adding__input');
+    //console.log(input.value);
+    if (input.value.length > 21) {
+        input.value = input.value.slice(0, 21) + "...";
+    }
+    movieDB.movies.push(input.value);
+    movieDB.movies.sort();
+    movieList.innerHTML = "";
+    movieDB.movies.forEach((film, i) => { 
+        movieList.innerHTML +=   `  
+            <li class="promo__interactive-item">${i+1}. ${film}
+            <div class="delete"></div>
+            </li>
+        `;         
+    });
+    if (favoriteFilm) {
+        console.log("Добавляем любимый фильм");
+    }
+});
+
+let some = 0,
+    favoriteFilm = false;
+checkPoint.addEventListener('click', (e) => {
+    some++;
+    if (some%2 == 1) {
+        favoriteFilm = true;
+    } else {
+        favoriteFilm = false;
+    }
+});
+
+remover.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+        delete movieDB.movies[i];
+        movieDB.movies.sort();
+        movieList.innerHTML = "";
+        movieDB.movies.forEach((film, i) => { 
+        movieList.innerHTML +=   `  
+            <li class="promo__interactive-item">${i+1}. ${film}
+            <div class="delete"></div>
+            </li>
+        `;       
+    });
+        movieDB.movies.pop();
+});
 });
