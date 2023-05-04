ed2cccf837481b4789fae2750c1e97395bf6d378
Oleg <oleg25111@inbox.ru>
2023-05-04 17:56:41 +0300

    Mobile's event

--- js/script.js
+++ js/script.js
@@ -1,81 +1,22 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-    
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
-            "Скотт Пилигрим против..."
-        ]
-    };
-    
-    const adv = document.querySelectorAll('.promo__adv img'),
-          poster = document.querySelector('.promo__bg'),
-          genre = poster.querySelector('.promo__genre'),
-          movieList = document.querySelector('.promo__interactive-list'),
-          addForm = document.querySelector('form.add'),
-          addInput = addForm.querySelector('.adding__input'),
-          checkbox = addForm.querySelector('[type="checkbox"]');
+// События на мобильных устройствах
 
-    const deleteAdv = (arr) => {
-        arr.forEach(item => {
-            item.remove();
-        });
-    };
-    
-    addForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        let newFilm = addInput.value;
-        const favorite = checkbox.checked;
-        
-        if (newFilm) {
-            if (newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0, 22)}...`;
-            }
-            if (favorite) {
-                console.log("Добавляем любимый фильм");
-            }
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
-            createMovieList(movieDB.movies, movieList);
-        }
-        event.target.reset();
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        console.log('Start');
+        console.log(e.touches);
+        console.log(e.targetTouches);
     });
-
-    const makeChanges = () => {
-        genre.textContent = 'драма';
-        poster.style.backgroundImage = 'url("img/bg.jpg")';
-    };
-  
-    const sortArr = (arr) => {
-        arr.sort();
-    };
-    
-    function createMovieList(films, parent) {
-        parent.innerHTML = "";
-        sortArr(films);
-        films.forEach((film, i) => { 
-            parent.innerHTML +=   `  
-                <li class="promo__interactive-item">${i+1}. ${film}
-                <div class="delete"></div>
-                </li>
-            `;         
-        });
-        document.querySelectorAll('.delete').forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                btn.parentElement.remove();
-                movieDB.movies.splice(i, 1);
-                createMovieList(films, parent);
-            });
-        });
-    }
-
-    
-    makeChanges();
-    deleteAdv(adv);
-    createMovieList(movieDB.movies, movieList);
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+        console.log('Move');
+        console.log(e.targetTouches[0].pageX);
+    }); 
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+        console.log('End');
+    });
 });
